44de4f813e733585752c7449a0a3d30a4ae5b488
Taehyung(Daniel) Kim <57923188+dkim0827@users.noreply.github.com>
2021-01-24 13:49:17 -0800

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAUi7aUh8klXn0yYvekcScRKrX9cwTnV6k",
-  authDomain: "clone-b96ea.firebaseapp.com",
-  projectId: "clone-b96ea",
-  storageBucket: "clone-b96ea.appspot.com",
-  messagingSenderId: "628352570585",
-  appId: "1:628352570585:web:ec01685f272812703adac8"
-};
